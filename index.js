fd348204394420271c057149b11be072c62b2b19
sandyhabala <sandycabal513@gmail.com>
2023-12-28 13:16:39 +0800

    add view movie

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const movieInfo = async (id) => {
   return movieInfo;
 };
 
-const getSimilarMovies = async (id) => {
+const relatedMovies = async (id) => {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}&language=en-US&page=1`
   );
@@ -43,7 +43,7 @@ const getSimilarMovies = async (id) => {
 
 const moviePoster = (title, backdrop_path, id) => {
   return `
-        <div class="movie-poster" onclick="togglePopup(${id})">
+        <div class="movie-poster" onclick="viewMovie(${id})">
             <div class="movie-poster-content">
               <img src="http://image.tmdb.org/t/p/w500${backdrop_path}" alt=${backdrop_path}>
               <h3>${title}</h3>
@@ -62,3 +62,73 @@ const renderMovies = async () => {
 };
 
 renderMovies();
+
+const popupContent = (
+  title,
+  overview,
+  popularity,
+  backdrop_path,
+  vote_count,
+  vote_average
+) => {
+  return `
+    <div class="popup-content">
+      <div>
+        <h1>${title}</h1>
+
+        
+        <h4>Overview:</h4>
+        <p>${overview}</p> 
+        
+        <h4>Popularity</h4>
+        <h5>${popularity}</h5>
+        
+        <h4>Votes:</h4>
+        <p>${vote_count}</p>
+
+        <h4>Vote average:</h4>
+        <p>${vote_average}</p>
+      </div>
+      <img src="http://image.tmdb.org/t/p/w500${backdrop_path}">
+    </div>
+    
+  `;
+};
+
+const moviePopup = document.getElementById("movie-popup");
+const popupOverlay = document.getElementById("popup-overlay");
+const popupContainer = document.getElementById("popup-container");
+const relatedMoviesContainer = document.getElementById("related-movies");
+
+const viewMovie = async (id) => {
+  moviePopup.style.display = "block";
+  popupOverlay.style.display = "block";
+  console.log(id);
+  currentMovie = await movieInfo(id);
+
+  popupContainer.innerHTML = popupContent(
+    currentMovie.title,
+    currentMovie.overview,
+    currentMovie.popularity,
+    currentMovie.backdrop_path,
+    currentMovie.vote_count,
+    currentMovie.vote_average
+  );
+
+  const similarMovies = await relatedMovies(id);
+
+  console.log(similarMovies);
+
+  const mappedMovies = similarMovies.map(({ title, backdrop_path, id }) => {
+    return moviePoster(title, backdrop_path, id);
+  });
+
+  relatedMoviesContainer.innerHTML = mappedMovies.join("");
+
+  console.log(currentMovie);
+};
+
+const closeMoviePopup = () => {
+  moviePopup.style.display = "none";
+  popupOverlay.style.display = "none";
+};
